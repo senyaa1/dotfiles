9f761ae195e0e1daf638e6828f73fe93f8bdd577
Arseniy Semenov <_senyaa@tutanota.com>
2024-09-01 16:32:38 +0300

    Update PKGLIST

---
+++ README.md
@@ -0,0 +1,3 @@
+# Arch dotfiles
+
+These are my dotfiles for my Arch linux setup. They contain configuration and auto-install scripts for the software I am using.
--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+# Install font
+echo "Downloading fonts"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/CascadiaCode.zip --output-document /tmp/CaskaydiaCove.zip
+
+sudo unzip -o /tmp/CaskaydiaCove.zip -d /usr/share/fonts/
+sudo rm -f /usr/share/fonts/LICENSE
+sudo rm -f /usr/share/fonts/README.md
+
+rm -f /tmp/CaskaydiaCove.zip
+
+sudo fc-cache -v
+
+# Install configs
 stow --target=$HOME user/
